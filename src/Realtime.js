140ceeb16c7c94b1fd5b8280e8276ba545b0a780
kittidaj6641 <kittidaj.im@ksu.ac.th>
2026-03-25 09:31:05 +0700

    Update Realtime.js

--- src/Realtime.js
+++ src/Realtime.js
@@ -15,8 +15,8 @@ import { ref, onValue } from "firebase/database";
 const getStatus = (type, value) => {
   const val = Number(value);
   if (type === 'ph') {
-    if (val < 6.5 || val > 9.0) return 'danger';
-    if (val < 6.0 || val > 8.5) return 'warning';
+    if (val < 6.0 || val > 9.0) return 'danger';
+    if (val < 6.5 || val > 8.5) return 'warning';
     return 'normal';
   }
   if (type === 'do') {
